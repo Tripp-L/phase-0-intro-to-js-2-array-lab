e9a05cf584f96df01f1824daf54edf12fa8c65c1
trippl <tlaursen8@yahoo.com>
2024-04-02 12:19:29 -0400

    intro to JS 2 array lab

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-const cats = ['Milo', 'Otis', 'Garfield']
+const cats = ["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendCat(name) {
-    cats.push(name)
+    cats.push(name) 
 }
+
 function destructivelyPrependCat(name) {
     cats.unshift(name)
-} 
+}
+
 function destructivelyRemoveLastCat() {
     cats.pop()
 }
+
 function destructivelyRemoveFirstCat() {
     cats.shift()
 }
+
 function appendCat(name) {
     let newCatArray = [...cats]
     newCatArray.push(name)
     return newCatArray
 }
+
 function prependCat(name) {
     let newCatArray = [...cats]
     newCatArray.unshift(name)
     return newCatArray
 }
+
 function removeLastCat() {
     let newCatArray = [...cats]
     newCatArray.pop()
     return newCatArray
 }
-function removeFirstCat() {
+
+function removeFirstCat(){
     let newCatArray = [...cats]
     newCatArray.shift()
     return newCatArray
 }
+
 
 
 
